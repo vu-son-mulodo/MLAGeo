398632b7c60fb5f310860d131d48b3c8240bab5a
vu-son-mulodo <vu.son@mulodo.com>
2013-09-25 17:25:31 +0700

    commit 4

---
+++ TestTravisRobotium/src/mulodo/com/travisrobotium/test/TestMainActivity.java
@@ -0,0 +1,25 @@
+package mulodo.com.travisrobotium.test;
+
+import mulodo.com.travisrobotium.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class TestMainActivity extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	public TestMainActivity() {
+		super(MainActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+}
